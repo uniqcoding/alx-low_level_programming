9bc1a925b7d73fce59b3785b0976e073bb13707a
uniqcoding <uniqcoding@gmail.com>
2022-09-08 14:49:53 -0700

    Puts code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#<stdio.h>
+
+  /*main prints the string in the put function
+   *Using the main function, this program prints 
+   *"Programming is like building a multilingual puzzle."
+   *Return: 0
+   */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle.");
+  return(0);
+}
