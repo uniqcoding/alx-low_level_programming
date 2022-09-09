2a155789b1039a02f338f12c0cf90ef417c89d63
uniqcoding <uniqcoding@gmail.com>
2022-09-09 15:08:37 -0700

    alphaBET3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,8 +5,9 @@
 *
 * Description: using main function
 * this program prints the alphabet in lowercase then in uppercase
+* Return: 0
 */
-int main (void)
+int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= z; ch++)
