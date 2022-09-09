4dbe7c98840159ade4fd374427e4a1a1ee17719e
uniqcoding <uniqcoding@gmail.com>
2022-09-09 12:57:02 -0700

    Last digit new2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 l = n % 10;
 
-if (l > 5)  
-{     
-  printf("Last digit of %d is %d and is greater than 5\n", n, l);     
-}
- else if (l == 0)
+if (l > 5)
 {
-  printf("Last digit of %d is %d and is 0\n", n, l);
+printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
- else
+else if (l == 0)
 {
-  printf("Last digit of %d is %d and is less than and not 0\n", n, l);
-  return (0)
+printf("Last digit of %d is %d and is 0\n", n, l);
 }
- 
+else  
+{    
+printf("Last digit of %d is %d and is less than and not 0\n", n, l);     
+return (0);
+}
