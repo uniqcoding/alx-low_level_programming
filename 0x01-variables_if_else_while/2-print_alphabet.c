c0b8669f6ea7d71f18bd839aa5f58a2d0947234c
uniqcoding <uniqcoding@gmail.com>
2022-09-09 14:27:52 -0700

    PutChar2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,17 @@
 /**
 * main - print the alphabet in lowercase followed by a new line
 *
-* Description: using main function 
+* Description: using main function
 * this program prints the alphabet in lower case
 * Return: 0
 */
 int main(void)
 {
-  char ch;
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-    {
-      putchar(ch);
-    }
-  putchar('\n');
-  return (0);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
