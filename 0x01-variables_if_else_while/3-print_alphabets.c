5fe849f3f2ae3153f9eb7481636aea6423026601
uniqcoding <uniqcoding@gmail.com>
2022-09-09 14:59:48 -0700

    alphaBET1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-* main - print the alphabet in lowercase then in uppercase followed by a new line
+* main - print the alphabet in lowercase then in uppercase followed
+* by a new line
 *
-* Description: using main function 
+* Description: using main function
 * this program prints the alphabet in lowercase then in uppercase
 */
 int main(void)
@@ -19,4 +20,3 @@ putchar(ch);
 putchar('\n');
 return (0);
 }
-  
