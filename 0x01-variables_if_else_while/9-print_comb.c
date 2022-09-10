902448b6e248f905e4e16c5e70b5045d2bf54934
uniqcoding <uniqcoding@gmail.com>
2022-09-09 17:07:47 -0700

    Nine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single digit numbers
+*
+* Description: using main function this progam prints
+* "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+* Return: 0
+*/
+int main(void)
+{
+int c;
+for ( c = 48; c <= 57; c++)
+{
+putchar (c);
+if (c != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
