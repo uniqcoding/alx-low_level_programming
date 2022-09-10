6cab34ed7f99348e838cbfbccee3f9a3f4447773
uniqcoding <uniqcoding@gmail.com>
2022-09-09 18:00:59 -0700

    Last digit1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,11 +23,11 @@ printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 else if (l == 0)
 {
-printf ("Last digit of %d is %d and is 0\n", n, l);
+printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
 {
 printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, l);
 }
- return (0);
+return (0);
 }
