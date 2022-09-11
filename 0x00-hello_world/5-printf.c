6059b897a9e80bc0539ae9d84d2abed401307a1a
uniqcoding <uniqcoding@gmail.com>
2022-09-11 01:06:40 -0700

    5 Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print a string
+*
+* Description: using main function print a
+* string using printf
+* Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
