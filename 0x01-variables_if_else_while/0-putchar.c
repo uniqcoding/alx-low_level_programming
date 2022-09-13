148f23c8c8788c1a87d825e0bb184c5c79a03869
uniqcoding <uniqcoding@gmail.com>
2022-09-13 12:49:26 -0700

    Putchar

--- 0x01-variables_if_else_while/0-putchar.c
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * main - print _putchar
- * Return - Always 0
- */
+* main - print _putchar
+* Return - Always 0
+*/
 
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
-  return (0);
+return (0);
 }
