9885607c0143a7fa0d82bf6bc7d199389f27fcd6
uniqcoding <uniqcoding@gmail.com>
2022-09-13 20:13:40 -0700

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - function to print alphabet
+* Return: 0
+*/
+void print_alphabet(void);
+{
+char c;
+
+for(c = 'a'; c <= 'z'; c++);
+{
+_putchar(c);
+}
+return (0);
+}
