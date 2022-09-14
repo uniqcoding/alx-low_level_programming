4cb197ddece131b6b4e5e16fd96ad08c7183000a
uniqcoding <uniqcoding@gmail.com>
2022-09-13 23:53:36 -0700

    Print to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
 * print_to_98 - prints all natural numbers from n to 98
 * in order separated by a comma followed by space.
@@ -6,12 +6,14 @@
 */
 void print_to_98(int n)
 {
+
 if (n >= 98)
 {
 while (n > 98)
 printf("%d, ", n--);
 printf("%d\n", n);
 }
+
 else
 {
 while (n < 98)
