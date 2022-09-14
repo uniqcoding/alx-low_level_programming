98d9c6cfb381b09140db0073b7f7d388dcbe7c92
uniqcoding <uniqcoding@gmail.com>
2022-09-13 23:34:05 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - add two integers
+* @num1: First integer to be added
+* @num2: Second integer to be added
+* Return: Result of addition
+*/
+int add(int num2, int num1)
+{
+return (num1 + num2);
+}
