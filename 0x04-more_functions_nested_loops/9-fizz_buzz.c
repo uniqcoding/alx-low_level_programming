e4f3df09bf77bff8f83a6d17b47fafb6efc9c273
uniqcoding <uniqcoding@gmail.com>
2022-09-15 19:36:58 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int num = 1;
 
 while (num++ < 100)
 {
-if ((num % 3) == 0)
-{
-printf("Fizz ");
-}
-else if ((num % 3 == 0) && (num % 5 == 0))
+if ((num % 15) == 0)
 {
 printf("FizzBuzz ");
+}
+else if ((num % 3 == 0)
+{
+printf("Fizz ");
 }
 else if ((num % 5) == 0)
 {
