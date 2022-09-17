e5832c6271ca22f1c54bc6b72051c7d74868be83
uniqcoding <uniqcoding@gmail.com>
2022-09-16 21:36:34 -0700

    Prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - print largest prime factor
+* Return: 0
+*/
+
+int main(void)
+{
+long number = 612852475143;
+int icc;
+
+while (icc++ < number / 2)
+{
+if (number % icc == 0)
+{
+number /= 2;
+continue;
+}
+
+for (icc = 3; icc < number / 2; icc += 2)
+{
+if (number % icc == 0)
+number /= icc
+}
+}
+printf("%ld\n", number);
+return (0);
+}
