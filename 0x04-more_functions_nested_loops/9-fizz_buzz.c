a4cd04729fb2d3959e5312f2037daf1941cb52f8
uniqcoding <uniqcoding@gmail.com>
2022-09-16 21:18:52 -0700

    Abuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,41 +1,31 @@
 #include <stdio.h>
-
 /**
-* main - fizz buzz program
-*
+* main - function checks for uppercase character.
 * Return: 0
 */
-
 int main(void)
 {
-int num = 1;
+int h = 1;
 
-while (num++ < 100)
+while (h <= 100)
 {
-if ((num % 15) == 0)
-{
+if (h % 15 ==  0)
 printf("FizzBuzz ");
-}
-else if ((num % 3) == 0)
+else if (h % 5 == 0)
 {
-printf("Fizz ");
-}
-else if ((num % 5) == 0)
-{
-if (num != 100)
-{
-printf("Buzz ");
-}
-else
+if (h == 100)
 {
 printf("Buzz");
-}
+printf("\n");
 }
 else
-{
-printf("%d ", num);
+printf("Buzz ");
 }
+else if (h % 3 == 0)
+printf("Fizz ");
+else
+printf("%d ", h);
+h++;
 }
-printf("\n");
 return (0);
 }
