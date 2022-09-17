08096172312d0653b8126181d8b00aedb08a1ffd
uniqcoding <uniqcoding@gmail.com>
2022-09-16 21:00:39 -0700

    6Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - a program that prints size of types
 * Description - using main function the program prints
@@ -15,7 +16,7 @@ float f;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
 printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("Size of a lont int: %ld byte(s)\n", sizeof(li));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
