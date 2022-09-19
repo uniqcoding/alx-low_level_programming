28d6e8e71ad5bd6bbca3220f1a1d8522a3baae06
uniqcoding <uniqcoding@gmail.com>
2022-09-19 07:20:57 -0700

    Rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+* print_rev - prints string in reverse
+* @s: string to reverse
+* Return: nothing
+*/
+
+void print_rev(char *s)
+{
+int len = strlen(*s);
+while (len--)
+putchar(*(s + len));
+putchar(10);
+}
