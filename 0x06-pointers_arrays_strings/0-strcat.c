b8547c1a57920667bad2e7f78ee02643f0283e20
Meli <uniqcoding@gmail.com>
2022-09-22 08:34:29 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
- * _strcat - concatenate string pointed to by @src
- * to end of string pointed to by @dest
- * @dest: string to be appended
- * @src: string to be concatenated on
- * Return: returns pointer to @dest
+ * _strcat - concatenate string pointed to by
+ * @src to end of string pointed to by @dest
+ * @dest: string to append
+ * @src: string to be concatenated
+ *
+ * Return: pointer to @dest
  */
 
 char *_strcat(char *dest, char *src)
@@ -13,10 +13,10 @@ char *_strcat(char *dest, char *src)
 int index = 0, dest_len = 0;
 
 while (dest[index++])
-dest_len++;
+	dest_len++;
 
-for (index = 0; src[index] index++)
-dest[dest_len++] = src[index];
+for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index];
 
 return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@
 * char prototypes()
 */
 
-char *_strcat(char *dest, char *s);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -16,10 +16,10 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
-char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void reverse_array(int *a, int n);
 void print_number(int n);
-void print_buffer(char *b, int size);
+char *rot13(char *);
 
 #endif /*MAIN_H*/
