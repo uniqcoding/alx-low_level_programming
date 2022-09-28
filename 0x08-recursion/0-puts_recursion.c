58340ff7e42cbdd3f29b6343275df5ac5f3168d5
Meli <uniqcoding@gmail.com>
2022-09-28 05:13:44 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function to print string
+ * @s: string to be printed
+ * Return: returns string
+ */
+
+void _puts_recursion(char *s)
+{
+char s;
+
+printf("%c\n", s);
+return (s);
+}
