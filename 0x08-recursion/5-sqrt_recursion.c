379d860da7e4d77680b4c232da8f5d9a63ec2214
Meli <uniqcoding@gmail.com>
2022-09-28 09:21:14 -0700

    Squareroot with recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,19 +12,3 @@ if (n == 1 || n == 0)
 return (n);
 return (_sqrt(0, n));
 }
-
-/**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
- * Return: square root of n
- */
-
-int _sqrt(int n, int x)
-{
-if (n > x / 2)
-return (-1);
-else if (n * n == x)
-return (n);
-return (_sqrt(n + 1, x));
-}
