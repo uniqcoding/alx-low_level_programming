96ace5bff073559b2c22086c142b235ffa3a5d20
Meli <uniqcoding@gmail.com>
2022-09-29 07:44:46 -0700

    PrimeNumberGood

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,6 +19,7 @@ return (is_prime(n, start));
  * is_prime - returns 1 if n is prime
  * @n: number to check
  * @start: number to start checking from
+ * Return: 1 if n is prime, 0 otherwise
  */
 
 int is_prime(int n, int start)
