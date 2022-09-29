878f9e850b25bb897b3252a8ec2e5adad21e96be
Meli <uniqcoding@gmail.com>
2022-09-29 00:56:03 -0700

    Natural Square Root Good

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,10 +5,25 @@
  * @n: number to use
  * Return: square root of n
  */
-
 int _sqrt_recursion(int n)
 {
 if (n == 1 || n == 0)
-return (n);
+	return (n);
 return (_sqrt(0, n));
+}
+
+/**
+ * _sqrt - returns the square root
+ * @n: test number
+ * @x: squared number
+ * Return: square root of n
+ */
+
+int _sqrt(int n, int x)
+{
+if (n > x / 2)
+	return (-1);
+else if (n * n == x)
+	return (n);
+return (_sqrt(n + 1, x));
 }
