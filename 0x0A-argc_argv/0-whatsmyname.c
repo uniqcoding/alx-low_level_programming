677de31ff3c787808e526205fe67dd282af5f2ef
Meli <uniqcoding@gmail.com>
2022-10-03 21:04:00 -0700

    PrintItsName

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void)argc;
-printf("My program name is: %s\n", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
