7bc1ebb37c45b4a96231214a5026d75c50121d41
Meli <uniqcoding@gmail.com>
2022-10-07 01:35:20 -0700

    Concatenate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,13 +18,13 @@ if (s1 == NULL)
 	i = 0;
 else
 {
-for (i = 0; s1[i]; i++)
+for (i = 0; s1[i]; i++);
 }
 if (s2 == NULL)
 	j = 0;
 else
 {
-for (j = 0; s2[j]; j++)
+for (j = 0; s2[j]; j++);
 }
 if (j > n)
 j = n;
